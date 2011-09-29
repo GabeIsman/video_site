ec6d9d616a75c398bac3d9a9016948cdeef1dcf2
Gabe Isman <gabe.isman@gmail.com>
2011-09-29 00:13:17 -0400

    video model

---
+++ db/migrate/20110929040717_create_videos.rb
@@ -0,0 +1,10 @@
+class CreateVideos < ActiveRecord::Migration
+  def change
+    create_table :videos do |t|
+      t.text :description
+      t.string :name
+      t.string :url
+      t.timestamps
+    end
+  end
+end
