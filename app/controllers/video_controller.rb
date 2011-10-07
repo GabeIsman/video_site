08fa9bf577b8b103ffbff165344e2b7f4d2858e6
Gabe Isman <gabe.isman@gmail.com>
2011-10-06 22:56:26 -0400

    The simplest CMS ever, accessible at /video/edit/:id

--- app/controllers/video_controller.rb
+++ app/controllers/video_controller.rb
@@ -6,4 +6,32 @@ class VideoController < ApplicationController
   def all
     @videos = Video.all
   end
+
+  def view
+    id = params[:id]
+    @video = find_video(id)
+    return redirect_to :controller => :video, :action => :index unless @video
+  end
+
+  def edit
+    id = params[:id]
+    @video = find_video(id)
+    return redirect_to :controller => :video, :action => :index unless @video
+  end
+
+  def update
+    video = find_video(params[:video][:id])
+    return redirect_to :controller => :video, :action => :index unless video
+    video.update_attributes!(params[:video])
+  end
+
+  private
+
+  def find_video (id)
+    video = Video.find(id) rescue nil
+    if !video
+      flash[:error] = "We're sorry we couldn't find that video"
+    end
+    video
+  end
 end
