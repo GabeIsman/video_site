d258a8edd0441ef75f4a0e28eb698fe726f1c96d
Gabe Isman <gabe.isman@gmail.com>
2011-10-06 23:57:21 -0400

    View videos by tags

--- app/controllers/tag_controller.rb
+++ app/controllers/tag_controller.rb
@@ -1,2 +1,16 @@
 class TagController < ApplicationController
+  def index
+    @tags = Tag.all
+  end
+
+  def list
+    tag_name = params[:id] || ""
+    tag = Tag.find_by_name(tag_name.titleize.downcase)
+    if !tag
+      flash[:error] = "Tag not found."
+      return redirect_to :controller => :tag, :action => :index
+    end
+    @videos = tag.videos
+    render '/video/all'
+  end
 end
