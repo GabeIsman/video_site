c8b34089ab3e3908dd1263948e0b3e0bfc028cd0
Gabe Isman <gabe.isman@gmail.com>
2011-10-18 02:23:47 -0400

    homepage videos are now deduped and limited to 3

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,6 +2,16 @@ class Tag < ActiveRecord::Base
   has_and_belongs_to_many :videos
 
   def self.homepage_tags
-    Tag.all.select{ |t| t.homepage == 1 }
+  	Tag.all.select{ |t| t.homepage > 0 }
   end
+
+	def homepage_videos( already_displayed_video_ids )
+  	candidates = videos.order(:created_at)
+		selected = []
+		candidates.each do |v|
+			selected << v unless already_displayed_video_ids.include?( v.id )
+			break if selected.count == 3
+		end
+		return selected
+	end
 end
